7c8bba8de612f0b69712ef4dbc05633861ab4da5
mhmitas <mahfuzulmitas@gmail.com>
2024-02-14 17:18:13 +0600

    3rd commit

--- scripts/alpha.js
+++ scripts/alpha.js
@@ -1,3 +1,15 @@
-function play(){
-    console.log('hello world')
+
+function continueGame(){
+    // step-1: generate a random alphabet;
+    const alphabet = getARandomAlphabet()
+    console.log('my random alphabet:',alphabet)
+
+    // set random alphabet to display
+    document.getElementById('alphabet-display').innerText = alphabet;
+}
+
+function play() {
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame()
 }
---
+++ scripts/utility.js
@@ -0,0 +1,20 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.add('hidden')
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden')
+}
+
+function getARandomAlphabet() {
+    // create an alphabet array
+    const alphabetStrings = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets = alphabetStrings.split('')
+    // get random index between (1-25)
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber)
+    const alphabet = alphabets[index]
+    return alphabet
+}
